bdffb7acd0fcae4d5552c1bd0136e9e4b6e4fd30
USBrido <obrido@gmail.com>
2019-12-06 02:22:06 +0000

    JSON the cat commit, needs clean up on the code *later*

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,26 +1,33 @@
 const request = require('request');
-const fs = require('fs');
 
-const args = process.argv;
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args[2]}`, (error, Response, body) => {
+const fetchBreedDescription = function (breedName, callback) {
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
-  if (error) {
-    console.log('Error Detected')
-    throw error
-  };
+    if (error) {
+      callback(error, null);
+    } else if (response.statusCode !== 200) {
+      const msg = `Page not found!`;
+      callback(Error(msg), null);
+    } else {
+      // get the descriptio from the data
+      const data = JSON.parse(body);
+      // console.log('DATA', data);
+      callback(null, data[0].description);
+    }
+  });
+};
 
-  const data = JSON.parse(body);
-
-  if (data.length === 0) {
-    console.log('Breed not found')
-    throw error
-  };
-
-  console.log(data[0].description);
-  console.log(typeof data);
+// if (error) {
+//   console.log('Error Detected')
+//   throw error
+// };
 
 
-});
+// if (data.length === 0) {
+//   console.log('Breed not found')
+//   throw error
+// };
 
-
-
+// console.log(data[0].description);
+// console.log(typeof data);
+module.exports = { fetchBreedDescription };
